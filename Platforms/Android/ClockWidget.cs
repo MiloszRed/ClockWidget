1d94e6128012e85836bd6bbc67b339278191a0e7
MiloszRed <308143@stud.umk.pl>
2025-01-20 13:46:58 +0100

    alarm manager without disabling

--- Platforms/Android/ClockWidget.cs
+++ Platforms/Android/ClockWidget.cs
@@ -2,6 +2,7 @@
 using Android.Appwidget;
 using Android.Content;
 using Android.Graphics;
+using Android.OS;
 using Android.Widget;
 
 namespace ZegarMauiWidget.Platforms.Android
@@ -11,6 +12,8 @@ namespace ZegarMauiWidget.Platforms.Android
     [MetaData("android.appwidget.provider", Resource="@xml/appwidget_provider")]
     public class ClockWidget : AppWidgetProvider
     {
+        private const int UPDATE_INTERVAL = 1000;
+
         public override void OnUpdate(Context? context, AppWidgetManager? appWidgetManager, int[]? appWidgetIds)
         {
             foreach (int appWidgetId in appWidgetIds)
@@ -21,6 +24,32 @@ namespace ZegarMauiWidget.Platforms.Android
 
                 appWidgetManager.UpdateAppWidget(appWidgetId, views);
             }
+
+            SetAlarm(context);
+        }
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            base.OnReceive(context, intent);
+
+            if (intent.Action == AppWidgetManager.ActionAppwidgetUpdate)
+            {
+                AppWidgetManager appWidgetManager = AppWidgetManager.GetInstance(context);
+                ComponentName thisWidget = new ComponentName(context, Java.Lang.Class.FromType(typeof(ClockWidget)).Name);
+                int[] appWidgetIds = appWidgetManager.GetAppWidgetIds(thisWidget);
+
+                OnUpdate(context, appWidgetManager, appWidgetIds);
+            }
+        }
+
+        private static void SetAlarm(Context context)
+        {
+            AlarmManager alarmManager = (AlarmManager)context.GetSystemService(Context.AlarmService);
+            Intent intent = new Intent(context, typeof(ClockWidget));
+            intent.SetAction(AppWidgetManager.ActionAppwidgetUpdate);
+
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(context, 0, intent, PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Immutable);
+            alarmManager.SetExactAndAllowWhileIdle(AlarmType.ElapsedRealtimeWakeup, SystemClock.ElapsedRealtime() + UPDATE_INTERVAL, pendingIntent);
         }
     }
 }
